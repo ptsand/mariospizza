ee4f4789743ff29b8bd60c45e1c8f822161b7e7d
SimonBøghJørgensen <simo274e@stud.kea.dk>
2021-04-20 11:37:36 +0100

    Lavet Pizzas string om til en integer

--- main/src/MenuKort.java
+++ main/src/MenuKort.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 public class MenuKort {
     public static ArrayList listenOverPizzaerne() {
         ArrayList<Pizza> pizzaList = new ArrayList<>();
-        pizzaList.add(new Pizza("Nummer 0:", "Ost og skinke ", 65));
-        pizzaList.add(new Pizza("Nummer 1:", "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(0, "Ost og skinke ", 65));
+        pizzaList.add(new Pizza(1, "Ost og bacon ", 100));
 
         pizzaList.forEach(System.out::println);
 
