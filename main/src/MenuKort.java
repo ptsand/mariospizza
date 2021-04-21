5aad7bdae587e4be3268c3ee8a65dae6dd6366d9
simon <simo274e@stud.kea.dk>
2021-04-21 22:38:38 +0200

    Added pizzaer

--- main/src/MenuKort.java
+++ main/src/MenuKort.java
@@ -6,8 +6,34 @@ public class MenuKort {
         ArrayList<Pizza> pizzaList = new ArrayList<Pizza>();
         pizzaList.add(new Pizza(0, "Ost og skinke ", 65));
         pizzaList.add(new Pizza(1, "Ost og bacon ", 100));
-
-        // pizzaList.forEach(System.out::println);
+        pizzaList.add(new Pizza(2, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(3, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(4, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(5, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(6, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(7, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(8, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(9, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(10, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(11, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(12, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(13, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(14, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(15, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(16, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(17, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(18, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(19, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(20, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(21, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(22, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(23, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(24, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(25, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(26, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(27, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(28, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(29, "Ost og bacon ", 100));
 
         return pizzaList;
 
