688dabef79fda41aec4d39669eca34e819cd627c
simon <simo274e@stud.kea.dk>
2021-04-21 22:50:15 +0200

    Prøve

--- main/src/MenuKort.java
+++ main/src/MenuKort.java
@@ -4,9 +4,37 @@ import java.util.ArrayList;
 public class MenuKort {
     public static ArrayList listenOverPizzaerne() {
         ArrayList<Pizza> pizzaList = new ArrayList<Pizza>();
-        pizzaList.add(new Pizza(1, "Ost og bacon ", 100));
+        pizzaList.add(new Pizza(1, "Ost og bacon ", 65));
         pizzaList.add(new Pizza(2, "pepperoni med dressing ", 100));
-        pizzaList.add(new Pizza(2, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(3, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(4, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(5, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(6, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(7, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(8, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(9, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(10, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(11, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(12, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(13, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(14, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(15, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(16, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(17, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(18, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(19, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(20, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(21, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(22, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(23, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(24, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(25, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(26, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(27, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(28, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(29, "pepperoni med dressing ", 100));
+        pizzaList.add(new Pizza(30, "pepperoni med dressing ", 100));
+
         return pizzaList;
 
     }
