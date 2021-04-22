650d7ac579c31b180f28dac2a191456579445da4
simon <simo274e@stud.kea.dk>
2021-04-22 12:07:31 +0200

    Ændrede pizza nummer

--- main/src/MenuKort.java
+++ main/src/MenuKort.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 public class MenuKort {
     public static ArrayList listenOverPizzaerne() {
         ArrayList<Pizza> pizzaList = new ArrayList<Pizza>();
+        pizzaList.add(new Pizza(0, "Ost og bacon ", 65));
         pizzaList.add(new Pizza(1, "Ost og bacon ", 65));
         pizzaList.add(new Pizza(2, "pepperoni med dressing ", 100));
         pizzaList.add(new Pizza(3, "pepperoni med dressing ", 100));
@@ -33,7 +34,7 @@ public class MenuKort {
         pizzaList.add(new Pizza(27, "pepperoni med dressing ", 100));
         pizzaList.add(new Pizza(28, "pepperoni med dressing ", 100));
         pizzaList.add(new Pizza(29, "pepperoni med dressing ", 100));
-        pizzaList.add(new Pizza(30, "pepperoni med dressing ", 100));
+
 
         return pizzaList;
 
